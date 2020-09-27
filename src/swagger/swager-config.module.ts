6a8a34e6d9658dd08abb5224e997065a92e4a602
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-27 12:48:04 -0300

    fix: providing swagger config in swagger config module

--- src/swagger/swager-config.module.ts
+++ src/swagger/swager-config.module.ts
@@ -1,7 +1,8 @@
-import { Module } from "@nestjs/common";
-import { SwaggerConfigService } from "./core/swagger-service/swagger.service";
+import { Module } from '@nestjs/common';
+import { SwaggerConfigService } from './core/swagger-service/swagger.service';
 
 @Module({
-  exports: [SwaggerConfigService]
+  exports: [SwaggerConfigService],
+  providers: [SwaggerConfigService],
 })
-export class SwaggerConfigModule { }
+export class SwaggerConfigModule {}
