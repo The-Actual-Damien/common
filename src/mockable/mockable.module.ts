d29d8abe41a0e30514f4d2bbdaa7c2b584b35b4d
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-27 12:47:02 -0300

    fix: exporting and providing mocks in mockable module

--- src/mockable/mockable.module.ts
+++ src/mockable/mockable.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MockRepository } from '.';
+import { MockConfigService } from './core/config-service/mock-config.service';
+import { MockUseCase } from './core/use-case/mock.use-case';
 
 @Module({
-  providers: [MockRepository],
-  exports: [MockRepository],
+  providers: [MockRepository, MockUseCase, MockConfigService],
+  exports: [MockRepository, MockUseCase, MockConfigService],
 })
-export class MockableModule { }
+export class MockableModule {}
